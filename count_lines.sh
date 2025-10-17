45b44be15cdbe5df3303708f5b2eef66ecab0d21
Josh Wise <jdwisecomposer@gmail.com>
2025-10-16 20:42:20 -0500

    adjust line counting thing

--- count_lines.sh
+++ count_lines.sh
@@ -1,1 +1,1 @@
-git ls-files | grep -E -v '^(target|resources|debug_out)/' | xargs wc -l
+git ls-files | grep -E -v '^(target|resources|debug_out|.vscode)/|^Cargo\.lock$|^junk.rs$|^\.gitignore$' | xargs wc -l
