46464ca8bde6c2e0d7456a578e0273ec3df2bec8
Josh Wise <jdwisecomposer@gmail.com>
2025-11-18 00:54:25 -0600

    unequipped owned weapons now don't fall forever and don't render

--- src/items.rs
+++ src/items.rs
@@ -4,6 +4,7 @@ use crate::{entity_manager::EntityManager, physics::PhysicsState, util::data_str
 
 pub fn update(em: &mut EntityManager, ps: &mut PhysicsState) {
     orphaned_items_pass(em, ps);
+    unequipped_items_pass(em, ps);
 }
 
 //fn active_items_pass(em: &mut EntityManager) {
@@ -70,8 +71,29 @@ pub fn update(em: &mut EntityManager, ps: &mut PhysicsState) {
 //    }
 //}
 
-fn orphaned_items_pass(em: &mut EntityManager, ps: &mut PhysicsState) {
+fn unequipped_items_pass(em: &mut EntityManager, ps: &mut PhysicsState) {
+    let unequipped = em.get_all_unequipped_owned_ids();
+    
+    for id in unequipped.iter() {
+        if let Some(ph) = em.physics_handles.get(*id) {
+            let wrb = ps.rigid_body_set.get_mut(ph.rigid_body).unwrap();
+            let col = ps.collider_set.get_mut(ph.collider).unwrap();
 
+            if wrb.body_type() != RigidBodyType::KinematicPositionBased {
+                wrb.set_body_type(RigidBodyType::KinematicPositionBased, false);
+                wrb.enable_ccd(false);
+                wrb.wake_up(true);
+
+                col.set_sensor(false);
+                col.set_enabled(false);
+                
+                wrb.set_next_kinematic_position(Vec3::splat(0.0).into());
+            }
+        }
+    }
+}
+
+fn orphaned_items_pass(em: &mut EntityManager, ps: &mut PhysicsState) {
     let orphaned = em.get_all_orphaned_weapon_ids();
 
     for id in orphaned.iter() {
@@ -82,14 +104,13 @@ fn orphaned_items_pass(em: &mut EntityManager, ps: &mut PhysicsState) {
             if wrb.body_type() != RigidBodyType::Dynamic {
                 wrb.set_body_type(RigidBodyType::Dynamic, true);
                 wrb.set_gravity_scale(1.0, true);
-                wrb.enable_ccd(true); // TODO: Why does enabling ccd slow this down so much?
+                wrb.enable_ccd(true);
                 wrb.wake_up(true);
 
                 col.set_sensor(false);
                 col.set_density(50.0);
                 col.set_enabled(true);
-
-                wrb.apply_impulse(Vec3::new(0.0, 3.0, 0.0).into(), true);
+                // wrb.apply_impulse(Vec3::new(0.0, 3.0, 0.0).into(), true);
             }
         }
     }
