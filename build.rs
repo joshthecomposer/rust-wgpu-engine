5ff724b0dacd1883a599420761a54b22a8383370
Josh Wise <jdwisecomposer@gmail.com>
2025-12-06 23:26:30 -0600

    Update dependencies

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
 use std::{env, fs::{self, OpenOptions}, path::Path, process::Command};
 
 fn main() {
-    // Link against the GLFW static library
     #[cfg(target_os = "windows")]
     {
         println!("cargo:rustc-link-search=native=libs");
-        println!("cargo:rustc-link-lib=static=libclang");
-        println!("cargo:rustc-link-lib=static=glfw3");
-        println!("cargo:rustc-link-lib=static=assimp");
     }
     #[cfg(target_os = "macos")]
     {
--- poop.bat
+++
@@ -1,1 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat" && set CMAKE_GENERATOR=Ninja&& cargo run
