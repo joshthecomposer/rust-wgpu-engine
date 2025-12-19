c98915fd4b7a4c862ef031675c845fab14840a0f
Alex Howard <me@alexhoward.dev>
2025-12-18 23:51:39 -0600

    chore: math_utils

--- src/util/math_utils.rs
+++ src/util/math_utils.rs
@@ -1,13 +1,13 @@
-use glam::Vec3;
+// use glam::Vec3;
 
-pub fn normalize_to_white(v: Vec3) -> Vec3 {
-    // This sets the max val to 1.0 and scales the rest of the values along with it.
-    // This makes the overall color as bright as possible while maintaining the
-    // overall hue ratio
-    let max_val = v.max_element();
-    if max_val > 0.0 {
-        v / max_val
-    } else {
-        v
-    }
-}
+// pub fn normalize_to_white(v: Vec3) -> Vec3 {
+//     // This sets the max val to 1.0 and scales the rest of the values along with it.
+//     // This makes the overall color as bright as possible while maintaining the
+//     // overall hue ratio
+//     let max_val = v.max_element();
+//     if max_val > 0.0 {
+//         v / max_val
+//     } else {
+//         v
+//     }
+// }
