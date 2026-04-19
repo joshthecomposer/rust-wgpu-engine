9ffc8874ed757dde8020825d72cb8103ba10a9fe
joshthecomposer <jdwisecomposer@gmail.com>
2026-04-18 21:19:41 -0500

    update counting lines script

--- count_lines.sh
+++ count_lines.sh
@@ -1,1 +1,1 @@
-git ls-files | grep -E -v '^(target|resources|debug_out|.vscode)/|^Cargo\.lock$|^junk.rs$|^\.gitignore$' | xargs wc -l
+git ls-files | grep -E -v '^(target|resources|debug_out|.vscode|config)/|^Cargo\.lock$|^junk.rs$|^\.gitignore$' | xargs wc -l
