9e94b34b46bd44d2156ef7b197c5d9173ac9c7ac
joshthecomposer <jdwisecomposer@gmail.com>
2026-05-08 02:50:48 -0500

    Meme

--- src/status_effect_system.rs
+++ src/status_effect_system.rs
@@ -29,9 +29,7 @@ pub fn update(em: &mut EntityManager, dt: f32, _cmds: &mut CommandBuffer) {
                             effect.tick_accumulator -= *tick_interval;
 
                             let health = em.healths.get_mut(id).unwrap();
-
                             *health -= *damage_per_tick;
-                            println!("STATUS EFFECTTTTTT {}", health);
                         }
                     }
                     StatusEffectBehavior::StatModifier { stat, op, amount } => (),
